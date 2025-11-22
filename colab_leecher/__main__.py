eaff6d8fb61518a634199d089bb6996da1a50369
zoredeaxx <87483466+zoredeaxx@users.noreply.github.com>
2025-11-22 14:06:05 +0530

    Update __main__.py

--- colab_leecher/__main__.py
+++ colab_leecher/__main__.py
@@ -1,20 +1,17 @@
-# copyright 2024 © Xron Trix | https://github.com/Xrontrix10
-
-
-import logging, os
-from pyrogram import filters
+import logging, os, asyncio
+from pyrogram import Client, filters
 from datetime import datetime
-from asyncio import sleep, get_event_loop
-from colab_leecher import colab_bot, OWNER
-from colab_leecher.utility.handler import cancelTask
-from .utility.variables import BOT, MSG, BotTimes, Paths
-from .utility.task_manager import taskScheduler, task_starter
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from .utility.helper import isLink, setThumbnail, message_deleter, send_settings
 
+from colab_leecher import API_ID, API_HASH, BOT_TOKEN, OWNER
+from colab_leecher.utility.handler import cancelTask
+from colab_leecher.utility.variables import BOT, MSG, BotTimes, Paths
+from colab_leecher.utility.task_manager import taskScheduler, task_starter
+from colab_leecher.utility.helper import isLink, setThumbnail, message_deleter, send_settings
+
+colab_bot = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 src_request_msg = None
-
 
 @colab_bot.on_message(filters.command("start") & filters.private)
 async def start(client, message):
@@ -23,60 +20,44 @@ async def start(client, message):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    "Repository 🦄",
-                    url="https://github.com/XronTrix10/Telegram-Leecher",
-                ),
+                InlineKeyboardButton("Repository 🦄", url="https://github.com/XronTrix10/Telegram-Leecher"),
                 InlineKeyboardButton("Support 💝", url="https://t.me/Colab_Leecher"),
             ],
         ]
     )
     await message.reply_text(text, reply_markup=keyboard)
 
-
 @colab_bot.on_message(filters.command("leech") & filters.private)
 async def telegram_upload(client, message):
     global BOT, src_request_msg
     BOT.Mode.mode = "leech"
     BOT.Mode.ytdl = False
-
     text = "<b>⚡ Send Me DOWNLOAD LINK(s) 🔗»</b>\n\n🦀 Follow the below pattern\n\n<code>https//linktofile1.mp4\nhttps//linktofile2.mp4\n[Custom name space.mp4]\n{Password for zipping}\n(Password for unzip)</code>"
-
     src_request_msg = await task_starter(message, text)
-
 
 @colab_bot.on_message(filters.command("mirror") & filters.private)
 async def drive_upload(client, message):
     global BOT, src_request_msg
     BOT.Mode.mode = "mirror"
     BOT.Mode.ytdl = False
-
     text = "<b>⚡ Send Me DOWNLOAD LINK(s) 🔗»</b>\n\n🦀 Follow the below pattern\n\n<code>https//linktofile1.mp4\nhttps//linktofile2.mp4\n[Custom name space.mp4]\n{Password for zipping}\n(Password for unzip)</code>"
-
     src_request_msg = await task_starter(message, text)
-
 
 @colab_bot.on_message(filters.command("dirleech") & filters.private)
 async def directory_upload(client, message):
     global BOT, src_request_msg
     BOT.Mode.mode = "dir-leech"
     BOT.Mode.ytdl = False
-
     text = "<b>⚡ Send Me FOLDER PATH 🔗»</b>\n\n🦀 Below is an example\n\n<code>/home/user/Downloads/bot</code>"
-
     src_request_msg = await task_starter(message, text)
-
 
 @colab_bot.on_message(filters.command("ytleech") & filters.private)
 async def yt_upload(client, message):
     global BOT, src_request_msg
     BOT.Mode.mode = "leech"
     BOT.Mode.ytdl = True
-
     text = "<b>⚡ Send YTDL DOWNLOAD LINK(s) 🔗»</b>\n\n🦀 Follow the below pattern\n\n<code>https//linktofile1.mp4\nhttps//linktofile2.mp4\n[Custom name space.mp4]\n{Password for zipping}</code>"
-
     src_request_msg = await task_starter(message, text)
-
 
 @colab_bot.on_message(filters.command("settings") & filters.private)
 async def settings(client, message):
@@ -84,39 +65,30 @@ async def settings(client, message):
         await message.delete()
         await send_settings(client, message, message.id, True)
 
-
 @colab_bot.on_message(filters.reply)
 async def setPrefix(client, message):
     global BOT, SETTING
     if BOT.State.prefix:
         BOT.Setting.prefix = message.text
         BOT.State.prefix = False
-
         await send_settings(client, message, message.reply_to_message_id, False)
         await message.delete()
     elif BOT.State.suffix:
         BOT.Setting.suffix = message.text
         BOT.State.suffix = False
-
         await send_settings(client, message, message.reply_to_message_id, False)
         await message.delete()
-
 
 @colab_bot.on_message(filters.create(isLink) & ~filters.photo)
 async def handle_url(client, message):
     global BOT
-
-    # Reset
     BOT.Options.custom_name = ""
     BOT.Options.zip_pswd = ""
     BOT.Options.unzip_pswd = ""
-
     if src_request_msg:
         await src_request_msg.delete()
     if BOT.State.task_going == False and BOT.State.started:
         temp_source = message.text.splitlines()
-
-        # Check for arguments in message
         for _ in range(3):
             if temp_source[-1][0] == "[":
                 BOT.Options.custom_name = temp_source[-1][1:-1]
@@ -129,7 +101,6 @@ async def handle_url(client, message):
                 temp_source.pop()
             else:
                 break
-
         BOT.SOURCE = temp_source
         keyboard = InlineKeyboardMarkup(
             [
@@ -148,47 +119,33 @@ async def handle_url(client, message):
         )
     elif BOT.State.started:
         await message.delete()
-        await message.reply_text(
-            "<i>I am Already Working ! Please Wait Until I finish 😣!!</i>"
-        )
-
+        await message.reply_text("<i>I am Already Working ! Please Wait Until I finish 😣!!</i>")
 
 @colab_bot.on_callback_query()
 async def handle_options(client, callback_query):
     global BOT, MSG
-
     if callback_query.data in ["normal", "zip", "unzip", "undzip"]:
         BOT.Mode.type = callback_query.data
         await callback_query.message.delete()
-        await colab_bot.delete_messages(
-            chat_id=callback_query.message.chat.id,
-            message_ids=callback_query.message.reply_to_message_id,
-        )
+        await colab_bot.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.reply_to_message_id)
         MSG.status_msg = await colab_bot.send_message(
             chat_id=OWNER,
             text="#STARTING_TASK\n\n**Starting your task in a few Seconds...🦐**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("Cancel ❌", callback_data="cancel")],
-                ]
-            ),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Cancel ❌", callback_data="cancel")]]),
         )
         BOT.State.task_going = True
         BOT.State.started = False
         BotTimes.start_time = datetime.now()
-        event_loop = get_event_loop()
-        BOT.TASK = event_loop.create_task(taskScheduler())  # type: ignore
+        event_loop = asyncio.get_event_loop()
+        BOT.TASK = event_loop.create_task(taskScheduler())
         await BOT.TASK
         BOT.State.task_going = False
-
     elif callback_query.data == "video":
         keyboard = InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton("Convert", callback_data="convert-true"),
-                    InlineKeyboardButton(
-                        "Don't Convert", callback_data="convert-false"
-                    ),
+                    InlineKeyboardButton("Don't Convert", callback_data="convert-false"),
                 ],
                 [
                     InlineKeyboardButton("To » Mp4", callback_data="mp4"),
@@ -226,12 +183,8 @@ async def handle_options(client, callback_query):
     elif callback_query.data == "thumb":
         keyboard = InlineKeyboardMarkup(
             [
-                [
-                    InlineKeyboardButton("Delete Thumbnail", callback_data="del-thumb"),
-                ],
-                [
-                    InlineKeyboardButton("Go Back ⏎", callback_data="back"),
-                ],
+                [InlineKeyboardButton("Delete Thumbnail", callback_data="del-thumb")],
+                [InlineKeyboardButton("Go Back ⏎", callback_data="back")],
             ]
         )
         thmb_ = "None" if not BOT.Setting.thumbnail else "Exists"
@@ -243,105 +196,55 @@ async def handle_options(client, callback_query):
         if BOT.Setting.thumbnail:
             os.remove(Paths.THMB_PATH)
         BOT.Setting.thumbnail = False
-        await send_settings(
-            client, callback_query.message, callback_query.message.id, False
-        )
+        await send_settings(client, callback_query.message, callback_query.message.id, False)
     elif callback_query.data == "set-prefix":
-        await callback_query.message.edit_text(
-            "Send a Text to Set as PREFIX by REPLYING THIS MESSAGE »"
-        )
+        await callback_query.message.edit_text("Send a Text to Set as PREFIX by REPLYING THIS MESSAGE »")
         BOT.State.prefix = True
     elif callback_query.data == "set-suffix":
-        await callback_query.message.edit_text(
-            "Send a Text to Set as SUFFIX by REPLYING THIS MESSAGE »"
-        )
+        await callback_query.message.edit_text("Send a Text to Set as SUFFIX by REPLYING THIS MESSAGE »")
         BOT.State.suffix = True
-    elif callback_query.data in [
-        "code-Monospace",
-        "p-Regular",
-        "b-Bold",
-        "i-Italic",
-        "u-Underlined",
-    ]:
+    elif callback_query.data in ["code-Monospace", "p-Regular", "b-Bold", "i-Italic", "u-Underlined"]:
         res = callback_query.data.split("-")
         BOT.Options.caption = res[0]
         BOT.Setting.caption = res[1]
-        await send_settings(
-            client, callback_query.message, callback_query.message.id, False
-        )
-    elif callback_query.data in [
-        "convert-true",
-        "convert-false",
-        "mp4",
-        "mkv",
-        "q-High",
-        "q-Low",
-    ]:
+        await send_settings(client, callback_query.message, callback_query.message.id, False)
+    elif callback_query.data in ["convert-true", "convert-false", "mp4", "mkv", "q-High", "q-Low"]:
         if callback_query.data in ["convert-true", "convert-false"]:
-            BOT.Options.convert_video = (
-                True if callback_query.data == "convert-true" else False
-            )
-            BOT.Setting.convert_video = (
-                "Yes" if callback_query.data == "convert-true" else "No"
-            )
+            BOT.Options.convert_video = True if callback_query.data == "convert-true" else False
+            BOT.Setting.convert_video = "Yes" if callback_query.data == "convert-true" else "No"
         elif callback_query.data in ["q-High", "q-Low"]:
             BOT.Setting.convert_quality = callback_query.data.split("-")[-1]
-            BOT.Options.convert_quality = (
-                True if BOT.Setting.convert_quality == "High" else False
-            )
-            await send_settings(
-                client, callback_query.message, callback_query.message.id, False
-            )
+            BOT.Options.convert_quality = True if BOT.Setting.convert_quality == "High" else False
+            await send_settings(client, callback_query.message, callback_query.message.id, False)
         else:
             BOT.Options.video_out = callback_query.data
-        await send_settings(
-            client, callback_query.message, callback_query.message.id, False
-        )
+        await send_settings(client, callback_query.message, callback_query.message.id, False)
     elif callback_query.data in ["media", "document"]:
         BOT.Options.stream_upload = True if callback_query.data == "media" else False
-        BOT.Setting.stream_upload = (
-            "Media" if callback_query.data == "media" else "Document"
-        )
-        await send_settings(
-            client, callback_query.message, callback_query.message.id, False
-        )
-
+        BOT.Setting.stream_upload = "Media" if callback_query.data == "media" else "Document"
+        await send_settings(client, callback_query.message, callback_query.message.id, False)
     elif callback_query.data == "close":
         await callback_query.message.delete()
     elif callback_query.data == "back":
-        await send_settings(
-            client, callback_query.message, callback_query.message.id, False
-        )
-
-    # @main Triggering Actual Leech Functions
+        await send_settings(client, callback_query.message, callback_query.message.id, False)
     elif callback_query.data in ["ytdl-true", "ytdl-false"]:
         BOT.Mode.ytdl = True if callback_query.data == "ytdl-true" else False
         await callback_query.message.delete()
-        await colab_bot.delete_messages(
-            chat_id=callback_query.message.chat.id,
-            message_ids=callback_query.message.reply_to_message_id,
-        )
+        await colab_bot.delete_messages(chat_id=callback_query.message.chat.id, message_ids=callback_query.message.reply_to_message_id)
         MSG.status_msg = await colab_bot.send_message(
             chat_id=OWNER,
             text="#STARTING_TASK\n\n**Starting your task in a few Seconds...🦐**",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton("Cancel ❌", callback_data="cancel")],
-                ]
-            ),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Cancel ❌", callback_data="cancel")]]),
         )
         BOT.State.task_going = True
         BOT.State.started = False
         BotTimes.start_time = datetime.now()
-        event_loop = get_event_loop()
-        BOT.TASK = event_loop.create_task(taskScheduler())  # type: ignore
+        event_loop = asyncio.get_event_loop()
+        BOT.TASK = event_loop.create_task(taskScheduler())
         await BOT.TASK
         BOT.State.task_going = False
-
-    # If user Wants to Stop The Task
     elif callback_query.data == "cancel":
         await cancelTask("User Cancelled !")
-
 
 @colab_bot.on_message(filters.photo & filters.private)
 async def handle_image(client, message):
@@ -351,66 +254,42 @@ async def handle_image(client, message):
         await msg.edit_text("**Thumbnail Successfully Changed ✅**")
         await message.delete()
     else:
-        await msg.edit_text(
-            "🥲 **Couldn't Set Thumbnail, Please Try Again !**", quote=True
-        )
-    await sleep(15)
+        await msg.edit_text("🥲 **Couldn't Set Thumbnail, Please Try Again !**", quote=True)
+    await asyncio.sleep(15)
     await message_deleter(message, msg)
-
 
 @colab_bot.on_message(filters.command("setname") & filters.private)
 async def custom_name(client, message):
     global BOT
     if len(message.command) != 2:
-        msg = await message.reply_text(
-            "Send\n/setname <code>custom_fileame.extension</code>\nTo Set Custom File Name 📛",
-            quote=True,
-        )
+        msg = await message.reply_text("Send\n/setname <code>custom_fileame.extension</code>\nTo Set Custom File Name 📛", quote=True)
     else:
         BOT.Options.custom_name = message.command[1]
-        msg = await message.reply_text(
-            "Custom Name Has Been Successfully Set !", quote=True
-        )
-
-    await sleep(15)
+        msg = await message.reply_text("Custom Name Has Been Successfully Set !", quote=True)
+    await asyncio.sleep(15)
     await message_deleter(message, msg)
-
 
 @colab_bot.on_message(filters.command("zipaswd") & filters.private)
 async def zip_pswd(client, message):
     global BOT
     if len(message.command) != 2:
-        msg = await message.reply_text(
-            "Send\n/zipaswd <code>password</code>\nTo Set Password for Output Zip File. 🔐",
-            quote=True,
-        )
+        msg = await message.reply_text("Send\n/zipaswd <code>password</code>\nTo Set Password for Output Zip File. 🔐", quote=True)
     else:
         BOT.Options.zip_pswd = message.command[1]
-        msg = await message.reply_text(
-            "Zip Password Has Been Successfully Set !", quote=True
-        )
-
-    await sleep(15)
+        msg = await message.reply_text("Zip Password Has Been Successfully Set !", quote=True)
+    await asyncio.sleep(15)
     await message_deleter(message, msg)
-
 
 @colab_bot.on_message(filters.command("unzipaswd") & filters.private)
 async def unzip_pswd(client, message):
     global BOT
     if len(message.command) != 2:
-        msg = await message.reply_text(
-            "Send\n/unzipaswd <code>password</code>\nTo Set Password for Extracting Archives. 🔓",
-            quote=True,
-        )
+        msg = await message.reply_text("Send\n/unzipaswd <code>password</code>\nTo Set Password for Extracting Archives. 🔓", quote=True)
     else:
         BOT.Options.unzip_pswd = message.command[1]
-        msg = await message.reply_text(
-            "Unzip Password Has Been Successfully Set !", quote=True
-        )
-
-    await sleep(15)
+        msg = await message.reply_text("Unzip Password Has Been Successfully Set !", quote=True)
+    await asyncio.sleep(15)
     await message_deleter(message, msg)
-
 
 @colab_bot.on_message(filters.command("help") & filters.private)
 async def help_command(client, message):
@@ -419,28 +298,24 @@ async def help_command(client, message):
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
+                [InlineKeyboardButton("Instructions 📖", url="https://github.com/XronTrix10/Telegram-Leecher/wiki/INSTRUCTIONS")],
                 [
-                    InlineKeyboardButton(
-                        "Instructions 📖",
-                        url="https://github.com/XronTrix10/Telegram-Leecher/wiki/INSTRUCTIONS",
-                    ),
-                ],
-                [
-                    InlineKeyboardButton(  # Opens a web URL
-                        "Channel 📣",
-                        url="https://t.me/Colab_Leecher",
-                    ),
-                    InlineKeyboardButton(  # Opens a web URL
-                        "Group 💬",
-                        url="https://t.me/Colab_Leecher_Discuss",
-                    ),
+                    InlineKeyboardButton("Channel 📣", url="https://t.me/Colab_Leecher"),
+                    InlineKeyboardButton("Group 💬", url="https://t.me/Colab_Leecher_Discuss"),
                 ],
             ]
         ),
     )
-    await sleep(15)
+    await asyncio.sleep(15)
     await message_deleter(message, msg)
 
+async def main():
+    await colab_bot.start()
+    logging.info("Colab Leecher Started!")
+    await asyncio.Event().wait()
 
-logging.info("Colab Leecher Started !")
-colab_bot.run()
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("Bot stopped.")
